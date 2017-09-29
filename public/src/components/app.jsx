a2cee08f7473bd88a937b39d96f9825a46c6cc7c
Jared Meek <jmeek13@gmail.com>
2017-09-29 11:31:21 -0700

    basic html scaffolding

--- public/src/components/app.jsx
+++ public/src/components/app.jsx
@@ -1,5 +1,45 @@
 import React from 'react';
 
-const App = () => <h1>Hello world!</h1>;
+const App = () => (
+  <div className="app-wrapper">
+    <div className="app-header">
+      <div className="app-header-title"><span>HealthMe</span></div>
+    </div>
+    <div className="app-full-window">
+      <div className="app-sidebar">
+        <div className="sidebar-entry"><span>Dashboard</span></div>
+        <div className="sidebar-entry"><span>Reports</span></div>
+        <div className="sidebar-entry"><span>Settings</span></div>
+        <div className="sidebar-entry"><span>Logout</span></div>
+      </div>
+      <div className="app-main-window">
+        <div className="form-bar-wrapper">
+          <div className="form-bar-button"><span>Feeling</span></div>
+          <div className="form-bar-button"><span>Meal</span></div>
+          <div className="form-bar-button"><span>Exercise</span></div>
+          <div className="form-bar-button"><span>Sleep</span></div>
+          <div className="form-bar-button"><span>Water</span></div>
+        </div>
+        <div className="dashboard-window">
+          <div className="report-tile report-tile-med">Report goes here</div>
+          <div className="report-tile report-tile-med">Report goes here</div>
+          <div className="report-tile report-tile-wide">Report goes here</div>
+        </div>
+      </div>
+    </div>
+    <div className="app-footer">
+      <div className="app-footer-content"><span>©2017 BeanieIO</span></div>
+    </div>
+  </div>
+);
+
+
+
+
+
+
+
+
+
 
 export default App;
