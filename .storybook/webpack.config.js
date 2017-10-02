de72c842ff89de8417f8ba6c29ea32bb859ceed1
Jared Meek <jmeek13@gmail.com>
2017-10-01 22:06:22 -0700

    updating storybook config to support css loading and font loading

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -16,8 +16,17 @@ module.exports = {
       {
         test: /\.css$/,
         loaders: ['style-loader', 'css-loader'],
-        include: path.resolve(__dirname, '../')        
-      }
+        include: [path.resolve(__dirname, '../'), path.resolve(__dirname, '../node_modules')]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loaders: ['file-loader'],
+        include: path.resolve(__dirname, '../')
+      },
+      {
+				test: /\.(gif|jpe?g|png)$/,
+				loader: 'url-loader?limit=25000'
+			}
     ]
   },
 };
