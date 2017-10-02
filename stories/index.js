e37fa09d1600a024bf0f32b77b71dbf2d257d0fc
ionajewel <ipjwilli@gmail.com>
2017-10-02 14:32:30 -0700

    reset story file to reduce chance of merge conflicts

--- stories/index.js
+++ stories/index.js
@@ -11,8 +11,6 @@ import '../public/styles/styles.css';
 import '../public/styles/app-styles.css';
 
 import PulseCheck from '../public/src/components/forms/pulse-check.jsx';
-import Meal from '../public/src/components/forms/meal.jsx';
-
 
 // storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
@@ -27,14 +25,6 @@ storiesOf('PulseCheck', module)
   .add('basic', () => (
     <div class="flex flex-center">
       <PulseCheck />
-    </div>
-  )
-  );
-
-storiesOf('Meal', module)
-  .add('basic', () => (
-    <div class="flex flex-center">
-      <Meal />
     </div>
   )
   );
