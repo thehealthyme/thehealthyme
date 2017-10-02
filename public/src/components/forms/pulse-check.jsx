6dd2c5d7cb9869fc761d6cff57454ce5e67add31
Jared Meek <jmeek13@gmail.com>
2017-10-01 22:28:46 -0700

    basic pulse check layout setup

--- public/src/components/forms/pulse-check.jsx
+++ public/src/components/forms/pulse-check.jsx
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import moment from 'moment';
 import momentLocalizer from 'react-widgets-moment';
-import DateTimePicker from 'react-widgets/lib/DateTimePicker';
-import Multiselect from 'react-widgets/lib/Multiselect';
+import { Multiselect, DateTimePicker } from 'react-widgets';
 import Rating from '../rating.jsx';
 import './form-styles.css';
+import 'react-widgets/dist/css/react-widgets.css';
+
+moment.locale('en');
+momentLocalizer();
 
 const physDefaults = ['Great All Around', 'Tired', 'Sore', 'Sick'];
 const emoDefaults = ['Energized', 'Relaxed', 'Stressed', 'Depressed'];
@@ -16,23 +19,47 @@ export default class PulseCheck extends Component {
     this.state = {
       phys: null,
       emo: null,
-      physTags: physDefaults, //TODO: serve this with an api call on mount
-      emoTags: emoDefaults, //TODO: serve this with an api call on mount
+      physOpts: physDefaults, //TODO: serve this with an api call on mount
+      emoOpts: emoDefaults, //TODO: serve this with an api call on mount
+      physTags: [],
+      emoTags: [],
     };
   }
 
   render() {
     return (
       <div className="form-wrapper shadow">
-        <div className="form-header">How are you feeling?</div>
+        <div className="form-header flex flex-align-center space-between">
+          <span>How are you feeling?</span>
+          <button type="button" class="close" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
         <form>
-          <div class="form-group flex flex-align-center">
-            <label class="form-label" for="phys-score">Physically:</label>
+          <div className="form-group flex flex-align-center">
+            <label className="form-label" htmlFor="phys-score">Physically:</label>
             <Rating onChange={v => this.setState({phys: v})}/>
           </div>
-          <div class="form-group flex flex-align-center">
-            <label class="form-label" for="phys-score">Emotionally:</label>
+          <div className="form-group flex flex-align-center">
+            <Multiselect className="form-multiselect" data={this.state.physOpts}
+              onChange={v => this.setState({physTags: v})} placeholder="Anything in particular?"
+            />
+          </div>
+          <div className="form-group flex flex-align-center">
+            <label className="form-label" htmlFor="phys-score">Emotionally:</label>
             <Rating onChange={v => this.setState({emo: v})}/>
+          </div>
+          <div className="form-group flex flex-align-center">
+            <Multiselect className="form-multiselect" data={this.state.emoOpts}
+              onChange={v => this.setState({emoTags: v})} placeholder="Anything in particular?"
+            />
+          </div>
+          <div className="form-group flex flex-align-center">
+            <label className="form-label" htmlFor="date">Date:</label>
+            <DateTimePicker id="date" className="form-datetimepicker"/>
+          </div>
+          <div className="form-submit-section flex flex-center">
+            <button type="submit" className="btn">Submit</button>
           </div>
         </form>
       </div>
