97d4cb9ae1eee6a1864cfcfcc73124e6cb037aec
Jared Meek <jmeek13@gmail.com>
2017-10-02 15:44:47 -0700

    toggle form and closeform wired up

--- public/src/components/dashboard.jsx
+++ public/src/components/dashboard.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 import PulseCheck from './forms/pulse-check.jsx';
+import Exercise from './forms/exercise.jsx';
+import classNames from 'classnames';
 import './dashboard.css';
 
 export default class Dashboard extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      openForm: '',
+    };
+
+    this.closeForm = this.closeForm.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+  }
+
+  toggleForm(form) {
+    this.setState({openForm: this.state.openForm === form ? '' : form});
+  }
+
+  closeForm() {
+    this.setState({openForm: ''});
+  }
+
+  renderForm(FormComponent, formName, mount) {
+    if (this.state.openForm === formName) {
+      const mountClass = classNames('entry-form-mount', {[mount === 'left']: 'entry-form-left', [mount === 'right']: 'entry-form-right'});
+      return (
+        <div className={mountClass}>
+          <FormComponent auth={this.props.getAuth} handleCancel={this.closeForm} />
+        </div>
+      );
+    }
   }
 
   render() {
@@ -17,11 +45,9 @@ export default class Dashboard extends Component {
           <div className="form-bar-button">
             <i className="mdi mdi-bike"></i>
           </div>
-          <div className="form-bar-button">
+          <div onClick={() => this.toggleForm('pulse')} className="form-bar-button form-bar-button-wide">
             <i className="mdi mdi-heart-pulse"></i>
-            <div className="entry-form-mount">
-              <PulseCheck />
-            </div>
+            {this.renderForm(PulseCheck, 'pulse')}
           </div>
           <div className="form-bar-button">
             <i className="mdi mdi-hotel"></i>
