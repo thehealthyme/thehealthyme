ba927e59643fc7161eb0fabd54ddc24ebba83c01
Jared Meek <jmeek13@gmail.com>
2017-10-02 11:26:07 -0700

    removed old code

--- public/src/components/sidenav.jsx
+++ public/src/components/sidenav.jsx
@@ -3,28 +3,24 @@ import { Link } from 'react-router-dom';
 import './sidenav.css';
 
 const navs = [
-  {label: 'Dashboard', icon: 'mdi-home-circle', link: '/dashboard'},
-  {label: 'Reports', icon: 'mdi-chart-line', link: '/reports'},
-  {label: 'Settings', icon: 'mdi-settings', link: '/settings'},
-  {label: 'Logout', icon: 'mdi-logout', link: '/logout'}
+  {label: 'dashboard', icon: 'mdi-home-circle', link: '/dashboard'},
+  {label: 'reports', icon: 'mdi-chart-line', link: '/reports'},
+  {label: 'settings', icon: 'mdi-settings', link: '/settings'},
+  {label: 'logout', icon: 'mdi-logout', link: '/logout'}
 ];
 
-const Sidenav = () => (
+const Sidenav = ({activeNav}) => (
   <div className="app-sidebar">
-    {navs.map((nav, i) => <NavLink key={`nav${i}`} link={nav.link} icon={nav.icon} />)}
+    {navs.map((nav, i) => (
+      <NavLink key={`nav${i}`} link={nav.link} icon={nav.icon} active={activeNav === nav.label} />
+    ))}
   </div>
 );
 
-const NavLink = ({link, icon}) => (
-  <Link to={link} className='sidebar-entry'>
+const NavLink = ({link, icon, active}) => (
+  <Link to={link} className={'sidebar-entry' + (active ? ' sidebar-active' : '')}>
     <i className={'mdi ' + icon}></i>
   </Link>
 );
 
 export default Sidenav;
-
-/* <div className="sidebar-entry"><i className="mdi mdi-home-circle"></i></div>
-  <Link to="home"></Link>
-  <div className="sidebar-entry sidebar-active"><i className="mdi mdi-chart-line"></i></div>
-  <div className="sidebar-entry"><i className="mdi mdi-settings"></i></div>
-<div className="sidebar-entry"><i className="mdi mdi-logout"></i></div> */
