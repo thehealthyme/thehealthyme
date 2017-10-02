fde564bc11485576875a926573b37e3577056105
Jared Meek <jmeek13@gmail.com>
2017-10-02 13:47:02 -0700

    applying active state from router params

--- public/src/components/sidenav.jsx
+++ public/src/components/sidenav.jsx
@@ -6,19 +6,20 @@ const navs = [
   {label: 'dashboard', icon: 'mdi-home-circle', link: '/dashboard'},
   {label: 'reports', icon: 'mdi-chart-line', link: '/reports'},
   {label: 'settings', icon: 'mdi-settings', link: '/settings'},
-  {label: 'logout', icon: 'mdi-logout', link: '/logout'}
+  {label: 'logout', icon: 'mdi-logout', link: '/'}
 ];
 
-const Sidenav = ({activeNav}) => (
+const Sidenav = ({match}) => (
   <div className="app-sidebar">
     {navs.map((nav, i) => (
-      <NavLink key={`nav${i}`} link={nav.link} icon={nav.icon} active={activeNav === nav.label} />
+      <NavLink key={`nav${i}`} link={nav.link} icon={nav.icon} active={match.params.nav === nav.label} />
     ))}
   </div>
 );
 
 const NavLink = ({link, icon, active}) => (
-  <Link to={link} className={'sidebar-entry' + (active ? ' sidebar-active' : '')}>
+  <Link to={link}
+    className={'sidebar-entry' + (active ? ' sidebar-active' : '')}>
     <i className={'mdi ' + icon}></i>
   </Link>
 );
