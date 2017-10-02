34b65ecc1acafeabfa6eed98d572fe3a72408d77
ionajewel <ipjwilli@gmail.com>
2017-10-02 10:02:01 -0700

    created meal form, checked appearance with storybook ok

---
+++ public/src/components/forms/meal.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import moment from 'moment';
+import momentLocalizer from 'react-widgets-moment';
+import { Multiselect, DateTimePicker } from 'react-widgets';
+import Rating from '../rating.jsx';
+import './form-styles.css';
+import 'react-widgets/dist/css/react-widgets.css';
+momentLocalizer();
+
+const ingredientsDefaults = ['peanuts', 'wheat', 'gluten', 'dairy', 'egg', 'soy'];
+
+export default class Meal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ingredientsOpts: ingredientsDefaults, //TODO: serve this with an api get on mount
+      ingredientsTags: [],
+      date: new Date(),
+    };
+  }
+
+  handleSubmit(e) {
+    e && e.preventDefault();
+    console.log('Submitting data: '); //TODO: wire this up to api post
+    console.log({
+      physTags: this.state.ingredientsTags,
+      date: this.state.date
+    });
+  }
+
+  handleCancel() {
+    if (!this.props.handleCancel) {
+      console.log('Error: missing cancel handler...'); //TODO: remove this once mounted
+    } else {
+      this.props.handleCancel();
+    }
+  }
+
+  render() {
+    return (
+      <div className="form-wrapper shadow">
+        <div className="form-header flex flex-align-center space-between">
+          <span>Select the ingredients you want to track for this meal.</span>
+          <button type="button" className="close" aria-label="Close" onClick={() => this.handleCancel()}>
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <div className="form-group flex flex-align-center">
+            <Multiselect className="form-multiselect" data={this.state.ingredientsOpts}
+              onChange={v => this.setState({ingredientsTags: v})} placeholder="Type or select ingredients here"
+              value={this.state.ingredientsTags}
+            />
+          </div>
+          <div className="form-group flex flex-align-center">
+            <DateTimePicker id="date" className="form-datetimepicker"
+              onChange={v => this.setState({date: v})} value={this.state.date}
+            />
+          </div>
+          <div className="form-submit-section flex flex-center">
+            <button type="submit" className="btn form-submit-btn shadow">Submit</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,8 @@ import '../public/styles/styles.css';
 import '../public/styles/app-styles.css';
 
 import PulseCheck from '../public/src/components/forms/pulse-check.jsx';
+import Meal from '../public/src/components/forms/meal.jsx';
+
 
 // storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
@@ -25,6 +27,14 @@ storiesOf('PulseCheck', module)
   .add('basic', () => (
     <div class="flex flex-center">
       <PulseCheck />
+    </div>
+  )
+  );
+
+storiesOf('Meal', module)
+  .add('basic', () => (
+    <div class="flex flex-center">
+      <Meal />
     </div>
   )
   );
