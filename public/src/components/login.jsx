6817eee94a9f5920b30d9d01b60acdf8a3ea4a96
ionajewel <ipjwilli@gmail.com>
2017-10-02 08:38:03 -0700

    add login, signup, and accompanying styles from fresh fork of repo

---
+++ public/src/components/login.jsx
@@ -0,0 +1,70 @@
+
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      formWarning: ''
+    };
+  }
+
+  handleSubmit(e) {
+    e && e.preventDefault();
+    if (this.validateForm(e)) {
+      axios.post('/api/users/login', {//TODO: be sure this endpoint matches server config, check back once db schema is finalized
+        username: this.state.username,
+        password: this.state.password,
+      }).then(resp => {
+        if (resp.status === 200 && resp.statusText === 'OK') { // TODO: make sure this statusText matches response from server
+          this.props.onLogin(resp.data.token);//TODO: make sure this prop is defined and passed down to this component
+          this.props.history.push({pathname: '/'});//TODO: make sure this prop is defined and passed down to this component
+        }
+      }).catch(err => this.setState({formWarning: 'Server error: ' + err}));//will this warning show up in form?
+    }
+  }
+
+  validateForm(e) {
+    if (this.state.password !== this.state.confPassword) {
+      this.setState({formWarning: 'Passwords must match, please try again.'});
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  render () {
+    return (
+      <div className="login-wrapper">
+        <div className="login-container">
+          <div className="login-title">Login To HealthMe</div>
+          <form class ="login-form" on-submit={e => this.handleSubmit(e)}>
+            <input
+              type="text"
+              className="login-form-control"
+              id="username"
+              placeholder="Username"
+              onChange={e => this.setState({'username': e.target.value})}
+            />
+            <input
+              type="password"
+              className="login-form-control"
+              id="password"
+              placeholder="Password"
+              onChange={e => this.setState({'password': e.target.value})}
+            />
+            <div>
+              <button type="submit" className="login-btn login-form-control">Login</button>
+            </div>
+            <div>
+              <a href="" className="login-link" onClick={(e) => this.goToSignup(e)}>Sign Up</a>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ public/src/components/signup.jsx
@@ -0,0 +1,83 @@
+
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      email: '',
+      password: '',
+      confPassword: '',
+      formWarning: ''
+    };
+  }
+
+
+  handleSubmit(e) {
+    e && e.preventDefault();
+    if (this.validateForm(e)) {
+      axios.post('/api/users/signup', {//TODO: be sure this endpoint matches server config, check back once db schema is finalized
+        username: this.state.username,
+        password: this.state.password,
+        email: this.state.email,
+      }).then(resp => {
+        if (resp.status === 201 && resp.statusText === 'Created') { // user successfully created
+          this.props.onLogin(resp.data.token);//TODO: make sure this prop is defined and passed down to this component
+          this.props.history.push({pathname: '/'});//TODO: make sure this prop is defined and passed down to this component
+        }
+      }).catch(err => this.setState({formWarning: 'Server error: ' + err}));//will this warning show up in form?
+    }
+  }
+
+  validateForm(e) {
+    if (this.state.password !== this.state.confPassword) {
+      this.setState({formWarning: 'Passwords must match, please try again.'});
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  render () {
+    return (
+      <div className="login-wrapper">
+        <div className="login-container">
+          <div className="login-title">Sign Up For HealthMe</div>
+          <form on-submit={e => this.handleSubmit(e)}>
+            <input
+              type="text"
+              className="login-form-control"
+              id="username"
+              placeholder="Username"
+              onChange={e => this.setState({username: e.target.value})}
+            />
+            <input
+              type="email"
+              className="login-form-control"
+              id="email"
+              placeholder="Email"
+              onChange={e => this.setState({'email': e.target.value})}
+            />
+            <input
+              type="password"
+              className="login-form-control"
+              id="password"
+              placeholder="Password"
+              onChange={e => this.setState({'password': e.target.value})}
+            />
+            <input
+              type="password"
+              className="login-form-control"
+              id="confPassword"
+              placeholder="Confirm password"
+              onChange={e => this.setState({'confPassword': e.target.value})}
+            />
+            <button type="submit" className="login-btn login-form-control">Sign Up</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
