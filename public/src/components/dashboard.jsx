5b28a4a1444727eb14aea0ba0df233d95691871e
Jared Meek <jmeek13@gmail.com>
2017-10-02 15:50:08 -0700

    rest of the forms wired up, fixed a classnames syntax error

--- public/src/components/dashboard.jsx
+++ public/src/components/dashboard.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import PulseCheck from './forms/pulse-check.jsx';
+import Meal from './forms/meal.jsx';
 import Exercise from './forms/exercise.jsx';
+import PulseCheck from './forms/pulse-check.jsx';
+import Sleep from './forms/sleep.jsx';
+import Water from './forms/water.jsx';
 import classNames from 'classnames';
 import './dashboard.css';
 
@@ -26,7 +29,7 @@ export default class Dashboard extends Component {
 
   renderForm(FormComponent, formName, mount) {
     if (this.state.openForm === formName) {
-      const mountClass = classNames('entry-form-mount', {[mount === 'left']: 'entry-form-left', [mount === 'right']: 'entry-form-right'});
+      const mountClass = classNames('entry-form-mount', {'entry-form-left': mount === 'left', 'entry-form-right': mount === 'right'});
       return (
         <div className={mountClass}>
           <FormComponent auth={this.props.getAuth} handleCancel={this.closeForm} />
@@ -39,21 +42,25 @@ export default class Dashboard extends Component {
     return (
       <div className="dashboard-container">
         <div className="form-bar-wrapper">
-          <div className="form-bar-button">
+          <div onClick={() => this.toggleForm('meal')} className="form-bar-button">
             <i className="mdi mdi-food"></i>
+            {this.renderForm(Meal, 'meal', 'left')}
           </div>
-          <div className="form-bar-button">
+          <div onClick={() => this.toggleForm('exercise')} className="form-bar-button">
             <i className="mdi mdi-bike"></i>
+            {this.renderForm(Exercise, 'exercise', 'left')}
           </div>
           <div onClick={() => this.toggleForm('pulse')} className="form-bar-button form-bar-button-wide">
             <i className="mdi mdi-heart-pulse"></i>
             {this.renderForm(PulseCheck, 'pulse')}
           </div>
-          <div className="form-bar-button">
+          <div onClick={() => this.toggleForm('sleep')} className="form-bar-button">
             <i className="mdi mdi-hotel"></i>
+            {this.renderForm(Sleep, 'sleep', 'right')}
           </div>
-          <div className="form-bar-button">
+          <div onClick={() => this.toggleForm('water')} className="form-bar-button">
             <i className="mdi mdi-water"></i>
+            {this.renderForm(Water, 'water', 'right')}
           </div>
         </div>
         <div className="dashboard-window">
