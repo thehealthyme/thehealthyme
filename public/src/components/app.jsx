3f088e26b12b075088aa3a2192010e0a7eaefed9
Jared Meek <jmeek13@gmail.com>
2017-10-02 10:11:49 -0700

    refactored app into a class comp

--- public/src/components/app.jsx
+++ public/src/components/app.jsx
@@ -1,53 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../../styles/app-styles.css';
 import PulseCheck from './forms/pulse-check.jsx';
 
-const App = () => (
-  <div className="app-wrapper">
-    <div className="app-header">
-      <div className="app-header-title"><span>HealthMe</span></div>
-    </div>
-    <div className="app-full-window">
-      <div className="app-sidebar">
-        <div className="sidebar-entry"><i className="mdi mdi-home-circle"></i></div>
-        <div className="sidebar-entry sidebar-active"><i className="mdi mdi-chart-line"></i></div>
-        <div className="sidebar-entry"><i className="mdi mdi-settings"></i></div>
-        <div className="sidebar-entry"><i className="mdi mdi-logout"></i></div>
-      </div>
-      <div className="app-main-window">
-        <div className="form-bar-wrapper">
-          <div className="form-bar-button">
-            <i className="mdi mdi-food"></i>
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return (
+      <div className="app-wrapper">
+        <div className="app-header">
+          <div className="app-header-title"><span>HealthMe</span></div>
+        </div>
+        <div className="app-full-window">
+          <div className="app-sidebar">
+            <div className="sidebar-entry"><i className="mdi mdi-home-circle"></i></div>
+            <div className="sidebar-entry sidebar-active"><i className="mdi mdi-chart-line"></i></div>
+            <div className="sidebar-entry"><i className="mdi mdi-settings"></i></div>
+            <div className="sidebar-entry"><i className="mdi mdi-logout"></i></div>
           </div>
-          <div className="form-bar-button">
-            <i className="mdi mdi-bike"></i>
-          </div>
-          <div className="form-bar-button">
-            <i className="mdi mdi-heart-pulse"></i>
-            <div className="entry-form-mount">
-              <PulseCheck />
+          <div className="app-main-window">
+            <div className="form-bar-wrapper">
+              <div className="form-bar-button">
+                <i className="mdi mdi-food"></i>
+              </div>
+              <div className="form-bar-button">
+                <i className="mdi mdi-bike"></i>
+              </div>
+              <div className="form-bar-button">
+                <i className="mdi mdi-heart-pulse"></i>
+                <div className="entry-form-mount">
+                  <PulseCheck />
+                </div>
+              </div>
+              <div className="form-bar-button">
+                <i className="mdi mdi-hotel"></i>
+              </div>
+              <div className="form-bar-button">
+                <i className="mdi mdi-water"></i>
+              </div>
+            </div>
+            <div className="dashboard-window">
+              <div className="report-tile shadow">Report goes here</div>
+              <div className="report-tile report-tile-wide shadow">Report goes here</div>
+              <div className="report-tile report-tile-wide shadow">Report goes here</div>
+              <div className="report-tile report-tile-wide shadow">Report goes here</div>
+              <div className="report-tile report-tile-wide shadow">Report goes here</div>
             </div>
           </div>
-          <div className="form-bar-button">
-            <i className="mdi mdi-hotel"></i>
-          </div>
-          <div className="form-bar-button">
-            <i className="mdi mdi-water"></i>
-          </div>
         </div>
-        <div className="dashboard-window">
-          <div className="report-tile shadow">Report goes here</div>
-          <div className="report-tile report-tile-wide shadow">Report goes here</div>
-          <div className="report-tile report-tile-wide shadow">Report goes here</div>
-          <div className="report-tile report-tile-wide shadow">Report goes here</div>
-          <div className="report-tile report-tile-wide shadow">Report goes here</div>
+        <div className="app-footer">
+          <div className="app-footer-content"><span>©2017 BeanieIo</span></div>
         </div>
       </div>
-    </div>
-    <div className="app-footer">
-      <div className="app-footer-content"><span>©2017 BeanieIo</span></div>
-    </div>
-  </div>
-);
-
-export default App;
+    );
+  }
+}
