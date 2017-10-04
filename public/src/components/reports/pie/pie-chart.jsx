207f1fccb09e7ffdeef9bcdecab86793e6745168
ionajewel <ipjwilli@gmail.com>
2017-10-04 12:22:03 -0700

    included piecelabel plugin for chart.js

--- public/src/components/reports/pie/pie-chart.jsx
+++ public/src/components/reports/pie/pie-chart.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Chart from 'chart.js';
+import PieceLabel from 'chart.piecelabel.js';
 
 const PieChart = () =>{
   var data = {
+    labels: ['A', 'B', 'C', 'D', 'E'],
     datasets: [{
       data: [10, 20, 30, 10, 5],
       backgroundColor: [
@@ -17,7 +19,41 @@ const PieChart = () =>{
   var ctx = 'pieChart';
   var chart = new Chart(ctx, {
     type: 'pie',
-    data: data
+    data: data,
+    options: {
+      pieceLabel: {
+        // render 'label', 'value', 'percentage', 'image' or custom function, default is 'percentage'
+        render: 'label',
+
+        // precision for percentage, default is 0
+        precision: 0,
+
+        // identifies whether or not labels of value 0 are displayed, default is false
+        showZero: false,
+
+        // font size, default is defaultFontSize
+        fontSize: 10,
+
+        // font color, can be color array for each data or function for dynamic color, default is defaultFontColor
+        fontColor: 'white',
+
+        // font style, default is defaultFontStyle
+        fontStyle: 'bold',
+
+        // font family, default is defaultFontFamily
+        fontFamily: "'Lato', Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+
+        // draw label in arc, default is false
+        arc: true,
+
+        // position to draw label, available value is 'default', 'border' and 'outside'
+        // default is 'default'
+        position: 'default',
+
+        // draw label even it's overlap, default is false
+        overlap: false,
+      }
+    }
   });
 
   return <canvas id="pieChart" width="400" height="400"></canvas>;
