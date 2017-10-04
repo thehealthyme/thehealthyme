8214670a2480cbd83648356ee85d1c4bfd038c64
Jared Meek <jmeek13@gmail.com>
2017-10-04 10:44:17 -0700

    setup for https server

--- app/index.js
+++ app/index.js
@@ -1,5 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
+const http = require('http');
 const app = require('./app.js');
-const port = process.env.PORT || 3000;
+
 const debug = process.env.DEBUG || false;
-app.listen(port);
-console.log('Server now listening on port ' + port);
+const httpPort = process.env.HTTP_PORT || 3000;
+const httpsPort = process.env.HTTPS_PORT || 3100;
+
+const httpsOptions = {
+  key: fs.readFileSync(path.join(__dirname, 'cert', 'domain.key')),
+  cert: fs.readFileSync(path.join(__dirname, 'cert', 'domain.crt'))
+};
+
+http.createServer(app).listen(httpPort);
+https.createServer(httpsOptions, app).listen(httpsPort);
+console.log('HTTP Server now listening on port ' + httpPort);
+console.log('HTTPS Server now listening on port ' + httpsPort);
