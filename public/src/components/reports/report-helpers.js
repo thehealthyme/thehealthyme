b444b679bd065da23b7e123b43960d1f10fab74e
Jared Meek <jmeek13@gmail.com>
2017-10-06 16:42:50 -0700

    switched filter to use isBetween

--- public/src/components/reports/report-helpers.js
+++ public/src/components/reports/report-helpers.js
@@ -9,6 +9,14 @@ export const aggregateCurrentWeek = (entries, field) => {
   return {week, totals};
 };
 
+export const aggregateAvgCurrentWeek = (entries, field) => {
+  entries = filterToCurrentWeek(entries);
+  let week = [0, 1, 2, 3, 4, 5, 6].map(n => moment().startOf('week').add(n, 'days'));
+  let totals = [0, 0, 0, 0, 0, 0, 0];
+  entries.forEach(entry => totals[moment(entry.datetime).weekday()] += entry[field]);
+  return {week, totals};
+};
+
 export const filterToCurrentWeek = (entries) => {
-  return _.filter(entries, entry => moment(entry.datetime).isSameOrAfter(moment().startOf('week')));
+  return _.filter(entries, entry => moment(entry.datetime).isBetween(moment().startOf('week'), moment().endOf('week'), 'day', '[]'));
 };
