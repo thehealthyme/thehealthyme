b12c693705cace267a958db1c2ada820611f01f4
Jared Meek <jmeek13@gmail.com>
2017-10-06 15:01:22 -0700

    added aggregation helpers for reports

---
+++ public/src/components/reports/report-helpers.js
@@ -0,0 +1,17 @@
+import moment from 'moment';
+import _ from 'lodash';
+
+const aggregateCurrentWeek = (entries) => {
+  entries = filterToCurrentWeek(entries);
+  let week = [0, 1, 2, 3, 4, 5, 6].map(n => moment().startOf('week').add(n, 'days'));
+  let totals = [0, 0, 0, 0, 0, 0, 0];
+  entries.forEach(entry => totals[moment(entry.datetime).weekday()] += entry.waterAmount);
+  return {week, totals};
+};
+
+const filterToCurrentWeek = (entries) => {
+  return _.filter(entries, entry => moment(entry.datetime).isSameOrAfter(moment.startOf('week')));
+};
+
+module.exports = filterToCurrentWeek;
+module.exports = aggregateCurrentWeek;
