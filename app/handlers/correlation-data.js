528f65d302167b0417b60e87d026ab88ae7a5e56
Jared Meek <jmeek13@gmail.com>
2017-10-05 22:47:35 -0700

    tweaked resulting data structure and some bug fixes

--- app/handlers/correlation-data.js
+++ app/handlers/correlation-data.js
@@ -1,33 +1,37 @@
 const _ = require('lodash');
 const moment = require('moment');
-const { User, Entry } = require('./models/models.js');
+const { User, Entry } = require('../models/models.js');
 const TWO_WEEKS = moment.duration(2, 'weeks');
 const ONE_MONTH = moment.duration(1, 'months');
 const TWELVE_HOURS = moment.duration(12, 'hours');
 
 const getCorrelationData = function(req, res) {
   const userId = req.user._id;
-  let resData = {Meal: [], Water: [], Exercise: [], Sleep: [], Pulse: []};
-  getEntriesForFeeling(userId, req.body.query.feeling, req.body.query.type)
+  let resData = {
+    raw: {Meal: [], Water: [], Exercise: [], Sleep: [], Pulse: []},
+    pulseMatches: [],
+    mealMatches: [],
+  };
+  getEntriesForFeeling(userId, req.query.feeling, req.query.type)
     .then(matches => {
-      resData.matches = matches;
+      resData.pulseMatches = matches;
       subqs = [];
       matches.forEach(entry => subqs.push(getPreceedingEntries(userId, entry, TWELVE_HOURS)));
       return Promise.all(subqs);
     }).then(subqRes => {
       results = _.uniqBy(_.flatten(subqRes), entry => entry._id.toString());
-      resData.meals = results.filter(r => r.type === 'Meal');
+      resData.mealMatches = results.filter(r => r.type === 'Meal');
     }).then(() => getLastEntries(userId, TWO_WEEKS))
     .then(entries => {
-      entries.forEach(entry => resData[entry.type] = entry);
+      entries.forEach(entry => resData.raw[entry.type].push(entry));
       res.status(200).json(resData);
     }).catch(err => console.log(err));
 };
 
 const getEntriesForFeeling = function (userId, feeling, feelingType) {
   return Entry.find({userId: userId})
-    .where(type).equals('Pulse')
-    .where([feelingType]).equals(feeling)
+    .where('type').equals('Pulse')
+    .where(feelingType).equals(feeling)
     .where('datetime').gt(moment().subtract(ONE_MONTH));
 };
 
@@ -37,8 +41,8 @@ const getLastEntries = function (userId, duration) {
 };
 
 const getPreceedingEntries = function(userId, sourceEntry, duration) {
-  let entryTime = moment(entry.datetime);
-  let startTime = entryTime.subtract(duration);
+  let entryTime = moment(sourceEntry.datetime);
+  let startTime = moment(entryTime).subtract(duration);
   return Entry.find({userId: userId}).where('datetime')
     .gte(startTime).lte(entryTime).exec();
 };
