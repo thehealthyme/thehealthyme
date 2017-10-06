787c9bb40dfc9879dd690454a18db5938df07e40
Jared Meek <jmeek13@gmail.com>
2017-10-06 15:07:47 -0700

    bugfix in filterToCurrentWeek and exporting properly now

--- public/src/components/reports/report-helpers.js
+++ public/src/components/reports/report-helpers.js
@@ -1,7 +1,7 @@
 import moment from 'moment';
 import _ from 'lodash';
 
-const aggregateCurrentWeek = (entries, field) => {
+export const aggregateCurrentWeek = (entries, field) => {
   entries = filterToCurrentWeek(entries);
   let week = [0, 1, 2, 3, 4, 5, 6].map(n => moment().startOf('week').add(n, 'days'));
   let totals = [0, 0, 0, 0, 0, 0, 0];
@@ -9,9 +9,6 @@ const aggregateCurrentWeek = (entries, field) => {
   return {week, totals};
 };
 
-const filterToCurrentWeek = (entries) => {
-  return _.filter(entries, entry => moment(entry.datetime).isSameOrAfter(moment.startOf('week')));
+export const filterToCurrentWeek = (entries) => {
+  return _.filter(entries, entry => moment(entry.datetime).isSameOrAfter(moment().startOf('week')));
 };
-
-module.exports = filterToCurrentWeek;
-module.exports = aggregateCurrentWeek;
